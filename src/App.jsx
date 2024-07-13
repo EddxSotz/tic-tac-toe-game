7d052ec998026d550453d715c698664fc99d6f3b
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2024-07-12 21:52:55 -0600

    Project setup - Starting files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <h1>React Tic-Tac-Toe</h1>
+      <div id="game-container">
+        <ol id="players">          
+        </ol>
+        <p>Game Board</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
