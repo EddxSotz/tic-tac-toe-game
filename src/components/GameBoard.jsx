fad5f3fb9c506edae2279e79d973de3a4aa817c6
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2024-07-20 01:34:59 -0600

    set an update board function

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ];
 
+console.log(initialGameBoard);
+
 function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleUpdateGameBoard(rowIndex, cellIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];          
+      updateGameBoard[rowIndex][cellIndex] = "X";      
+      return updateGameBoard;   
+    });    
+  }
+    
+
   return (    
       <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (         
-        <li key={rowIndex}>
+      {gameBoard.map((row, rowIndex) => (         
+        <li key={rowIndex}>          
           <ol>
             {row.map((cell, cellIndex) => (
-              <li key={cellIndex}>
-                <button>{cell}</button>
+              <li key={cellIndex}>                
+                <button onClick={() => handleUpdateGameBoard(rowIndex,cellIndex)}>{cell}</button>                              
               </li>
             ))}
           </ol> 
